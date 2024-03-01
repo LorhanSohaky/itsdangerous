941e78ff9544ea9b714963abe052c06d6ab61dd9
Hampus Kraft <hello@hampuskraft.com>
2024-03-01 20:52:44 +0100

    Fix exports for JSR

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export {base64Decode, base64Encode, bufferToInt, intToBuffer, wantBuffer} from './encoding.ts';
 export {BadData, BadHeader, BadPayload, BadSignature, BadTimeSignature, SignatureExpired} from './errors.ts';
-export {DefaultSerializer, Serializer, SerializerOptions} from './serializer.ts';
-export {HMACAlgorithm, KeyDerivation, NoneAlgorithm, Signer, SignerOptions, SigningAlgorithm} from './signer.ts';
+export {Serializer} from './serializer.ts';
+export type {DefaultSerializer, SerializerOptions} from './serializer.ts';
+export {HMACAlgorithm, KeyDerivation, NoneAlgorithm, Signer, SigningAlgorithm} from './signer.ts';
+export type {SignerOptions} from './signer.ts';
 export {TimedSerializer, TimestampSigner} from './timed.ts';
 export {URLSafeSerializer, URLSafeSerializerMixin, URLSafeTimedSerializer} from './url-safe.ts';
